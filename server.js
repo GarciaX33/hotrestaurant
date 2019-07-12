0d5d48c95d94547766b24cb637b5c644e1af5fc1
GarciaX33 <danny.garcia80@hotmail.com>
2019-07-12 14:22:32 -0500

    will add variables to hold data in server.js

--- server.js
+++ server.js
@@ -11,3 +11,55 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+// Variable that will hold the data (DATA)
+// =============================================================
+var tables = [
+    {
+        routeName: "tableone",
+        name: "table1",
+        user: "Daniel",
+        phone: 900,
+        email: "email@gmail.com"
+    },
+    {
+        routeName: "tabletwo",
+        name: "table2",
+        user: "Bri",
+        phone: 200,
+        email: "email@gmail.com"
+    },
+    {
+        routeName: "tablethree",
+        name: "table3",
+        user: "CJ",
+        phone: 55,
+        email: "email@gmail.com"
+    },
+    {
+        routeName: "tablefour",
+        name: "table4",
+        user: "Brooke",
+        phone: 200,
+        email: "email@gmail.com"
+    },
+    {
+        routeName: "tablefive",
+        name: "table5",
+        user: "name5",
+        phone: 200,
+        email: "email@gmail.com"
+    },
+];
+
+var waitlist = [
+    {
+        routeName: "tablesix",
+        name: "table6",
+        user: "name6",
+        phone: 900,
+        email: "email@gmail.com"
+    }
+
+];
