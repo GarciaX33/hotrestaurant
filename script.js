ea306b75c655005ba5cdc0694bc721969c1ae77e
GarciaX33 <danny.garcia80@hotmail.com>
2019-07-12 14:26:13 -0500

    will add variables of data into script

--- script.js
+++ script.js
@@ -9,6 +9,58 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Variable that will hold the data (DATA)
+// =============================================================
+var tables = [
+  {
+      routeName: "tableone",
+      name: "table1",
+      user: "Daniel",
+      phone: 900,
+      email: "email@gmail.com"
+  },
+  {
+      routeName: "tabletwo",
+      name: "table2",
+      user: "Bri",
+      phone: 200,
+      email: "email@gmail.com"
+  },
+  {
+      routeName: "tablethree",
+      name: "table3",
+      user: "CJ",
+      phone: 55,
+      email: "email@gmail.com"
+  },
+  {
+      routeName: "tablefour",
+      name: "table4",
+      user: "Brooke",
+      phone: 200,
+      email: "email@gmail.com"
+  },
+  {
+      routeName: "tablefive",
+      name: "table5",
+      user: "name5",
+      phone: 200,
+      email: "email@gmail.com"
+  },
+];
+
+var waitlist = [
+  {
+      routeName: "tablesix",
+      name: "table6",
+      user: "name6",
+      phone: 900,
+      email: "email@gmail.com"
+  }
+
+];
+
+
 app.use(express.static(path.join(__dirname, '../../assets')));
 
 app.get("/", function(req, res) {
